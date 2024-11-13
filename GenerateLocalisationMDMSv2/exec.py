45533d2106bf590ceedf5d61336a60a8463b0763
Gurjeet-eGov <gurjeet.singh@egovernments.org>
2024-11-13 15:56:30 +0530

    Updated scripts

---
+++ GenerateLocalisationMDMSv2/exec.py
@@ -0,0 +1,23 @@
+import RequestsCaller
+from localisation import *
+
+loc_obj=Localisation(FolderPath="SchemaDefs")
+loc_obj.generateLocalisations(module="rainmaker-workbench", locale="en_IN")
+
+LoginResponse=RequestsCaller.LoginAPI()
+Token=LoginResponse[0]
+UserReqInfo=LoginResponse[1]
+
+Localisations=loc_obj.LocalisationsList
+
+print("token: ", Token, "\nReq Info: ", UserReqInfo)
+
+LocalisationResponse=RequestsCaller.LocalisationUpsert(Token=Token,
+                                                       ReqInfo=UserReqInfo,
+                                                       LocalisationList=Localisations)
+print(LocalisationResponse.status_code)
+
+with open('response.json', 'w') as file:
+    file.write(json.dumps(LocalisationResponse.json()))
+
+# print(json.dumps(Localisations))
